fadb41b16c36727b34c89d2de8adf0a5379d689f
Filip Sarkowicz <fsarkowicz@gmail.com>
2023-09-24 11:47:09 +0200

    InventoryServiceTests Setup

--- RpgGameApiTests/ServicesTests/InventoryServiceTests.cs
+++ RpgGameApiTests/ServicesTests/InventoryServiceTests.cs
@@ -9,4 +9,23 @@ namespace RpgGameApiTests.ServicesTests;
 [TestFixture]
 public class InventoryServiceTests
 {
+    private IInventoryService _inventoryService;
+
+    private Mock<ICharacterRepository> _characterRepositoryMock;
+    private Mock<IInventoryRepository> _inventoryRepositoryMock;
+
+
+    [SetUp]
+    public void Setup()
+    {
+        _characterRepositoryMock = new Mock<ICharacterRepository>();
+        _inventoryRepositoryMock = new Mock<IInventoryRepository>();
+
+        _inventoryService = new InventoryService(_inventoryRepositoryMock.Object, _characterRepositoryMock.Object);
+    }
+
+    public void SetupInventoryService()
+    {
+        _inventoryService = new InventoryService(_inventoryRepositoryMock.Object, _characterRepositoryMock.Object);
+    }
 }
