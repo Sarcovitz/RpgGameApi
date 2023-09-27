f84b506cbef1134a399d1af92096e368a148a265
Filip Sarkowicz <fsarkowicz@gmail.com>
2023-09-27 18:12:49 +0200

    CreateAsync_OnSuccess_ReturnsCreateInventoryDTO test

--- RpgGameApiTests/ServicesTests/InventoryServiceTests.cs
+++ RpgGameApiTests/ServicesTests/InventoryServiceTests.cs
@@ -28,4 +28,52 @@ public class InventoryServiceTests
     {
         _inventoryService = new InventoryService(_inventoryRepositoryMock.Object, _characterRepositoryMock.Object);
     }
+
+    [Test]
+    public async Task CreateAsync_OnSuccess_ReturnsCreateInventoryDTO()
+    {
+        ulong userId = 123;
+        CreateInventoryRequest request = new()
+        {
+            CharacterId = 123,            
+        };
+
+        Character? foundCharacter = new()
+        {
+            UserId = userId,
+        };
+
+        Inventory? nullInventory = null;
+        Inventory? foundInventory = new()
+        {
+            CharacterId = 123,
+            Id = 123
+        };
+
+        CreateInventoryDTO expected = new()
+        {
+            Id = 123,
+            CharacterId = 123
+        };
+
+        _characterRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<ulong>()))
+            .ReturnsAsync(foundCharacter);
+
+        _inventoryRepositoryMock.Setup(x => x.GetByCharacterIdAsync(It.IsAny<ulong>(), It.IsAny<bool>()))
+            .ReturnsAsync(nullInventory);
+        _inventoryRepositoryMock.Setup(x => x.AddAsync(It.IsAny<Inventory>()))
+            .ReturnsAsync(foundInventory);
+
+        SetupInventoryService();
+
+        var result = await _inventoryService.CreateAsync(userId, request);
+
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result, Is.InstanceOf<CreateInventoryDTO>());
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Id, Is.EqualTo(expected.Id));
+            Assert.That(result.CharacterId, Is.EqualTo(expected.CharacterId));
+        });
+    }
 }
